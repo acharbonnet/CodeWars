9bd63664aa185c8361e954200803ddcf3cce60f4
acharbonnet <adriana.charbonnet@gmail.com>
2022-05-02 20:18:07 -0600

    created repo
    hello world

---
+++ 2022-05-02.js
@@ -0,0 +1,19 @@
+// function toWeirdCase(string){
+// 	let newList = [];
+//   let strList = string.split(" ");
+//  	for () { 
+//     for (let i=0; i < string.length; i++) {
+//       if (i%2 == 0) {
+//         newList = `${newString}${string[i].toUpperCase()}`;
+//       } else {
+//         newString = `${newString}${string[i].toLowerCase()}`;
+//       }
+//     }
+//   }
+//   return newString;
+// }
+
+// I realize this doesn't work, I'm just tired of looking at it. 
+// I hate everyone and everything
+
+console.log(toWeirdCase( "Weird string case" ));//=> returns "StRiNg")
