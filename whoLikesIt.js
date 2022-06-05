67cd4de9c431c3ba35432494d619906b5b50ae5f
acharbonnet <adriana.charbonnet@gmail.com>
2022-06-04 20:26:22 -0600

    forgot to save

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -31,3 +31,5 @@ function likes(names) {
   	return `${names[0]}, ${names[1]} and ${names.length -2} others like this`
   }
 }
+
+//This works, but how I have it written could also have been a case statement. 
